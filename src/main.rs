fbe7431ab89b463fcce57d98cafcf58bef5b8a6e
Thomas Mueller <thomasm@apache.org>
2023-11-05 12:28:15 +0100

    Correct XXHash32 calculation

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,8 @@ fn decompress(input_file_name: &str, output_file_name: &str) -> Result<usize, Er
         return error(format!("Unsupported block max size {block_max_size}").as_str());
     }
     let header_checksum = header[6];
-    let xxhash = (xxhash32(&header, 4, 2, 0) >> 8) & 0xff;
+    let mut hash = XXHash32::new(0);
+    let xxhash = (hash.update(&header, 4, 2)? >> 8) & 0xff;
     if xxhash as u8 != header_checksum {
         return error("Header checksum mismatch");
     }
@@ -211,6 +212,7 @@ fn xxhash32_file(input_file_name: &str) -> Result<u32, Error> {
     let block_size = 8 * 1024 * 1024;
     block.resize(block_size, 0);
     let mut hash = 0;
+    let mut state = XXHash32::new(0);
     while remaining > 0 {
         let read = if remaining < block_size as u64 {
             remaining as usize
@@ -219,65 +221,96 @@ fn xxhash32_file(input_file_name: &str) -> Result<u32, Error> {
         };
         reader.read_exact(&mut block[0..read])?;
         remaining -= read as u64;
-        hash = xxhash32(&block, 0, read, hash);
+        hash = state.update(&block, 0, read)?;
     }
     return Ok(hash);
 }
 
-fn xxhash32(buf: &Vec<u8>, start: usize, len: usize, seed: u32) -> u32 {
-    let prime1: u32 = 2654435761;
-    let prime2: u32 = 2246822519;
-    let prime3: u32 = 3266489917;
-    let prime4: u32 = 668265263;
-    let prime5 : u32 = 374761393;
-    let end = start + len;
-    let mut pos = start;
-    let mut h32;
-    if len >= 16 {
-        let limit = end - 16;
-        let mut v1 = seed.wrapping_add(prime1).wrapping_add(prime2);
-        let mut v2 = seed.wrapping_add(prime2);
-        let mut v3 = seed;
-        let mut v4 = seed.wrapping_sub(prime1);
-        loop {
-            v1 = read_u32_le(buf, pos).
-                wrapping_mul(prime2).wrapping_add(v1).
-                rotate_left(13).wrapping_mul(prime1);
-            v2 = read_u32_le(buf, pos + 4).
-                wrapping_mul(prime2).wrapping_add(v2).
-                rotate_left(13).wrapping_mul(prime1);
-            v3 = read_u32_le(buf, pos + 8).
-                wrapping_mul(prime2).wrapping_add(v3).
-                rotate_left(13).wrapping_mul(prime1);
-            v4 = read_u32_le(buf, pos + 12).
-                wrapping_mul(prime2).wrapping_add(v4).
-                rotate_left(13).wrapping_mul(prime1);
-            pos += 16;
-            if pos > limit {
-                break;
-            }
+const PRIME1: u32 = 2654435761;
+const PRIME2: u32 = 2246822519;
+const PRIME3: u32 = 3266489917;
+const PRIME4: u32 = 668265263;
+const PRIME5: u32 = 374761393;
+
+struct XXHash32 {
+    v1: u32,
+    v2: u32,
+    v3: u32,
+    v4: u32,
+    total: usize
+}
+
+impl XXHash32 {
+    fn new(seed: u32) -> XXHash32 {
+        XXHash32 {
+            v1: seed.wrapping_add(PRIME1).wrapping_add(PRIME2),
+            v2: seed.wrapping_add(PRIME2),
+            v3: seed,
+            v4: seed.wrapping_sub(PRIME1),
+            total: 0
         }
-        h32 = v1.rotate_left(1).
-            wrapping_add(v2.rotate_left(7)).
-            wrapping_add(v3.rotate_left(12)).
-            wrapping_add(v4.rotate_left(18));
-    } else {
-        h32 = seed.wrapping_add(prime5);
     }
-    h32 = h32.wrapping_add(len as u32);
-    while pos + 4 <= end {
-        h32 = read_u32_le(buf, pos).
-            wrapping_mul(prime3).wrapping_add(h32).
-            rotate_left(17).wrapping_mul(prime4);
-        pos += 4;
+
+    fn update(&mut self, buf: &Vec<u8>, start: usize, len: usize) -> Result<u32, Error> {
+        let end = start + len;
+        let mut pos = start;
+        if len >= 16 {
+            let limit = end - 16;
+            let mut v1 = self.v1;
+            let mut v2 = self.v2;
+            let mut v3 = self.v3;
+            let mut v4 = self.v4;
+            loop {
+                v1 = read_u32_le(buf, pos).
+                    wrapping_mul(PRIME2).wrapping_add(v1).
+                    rotate_left(13).wrapping_mul(PRIME1);
+                v2 = read_u32_le(buf, pos + 4).
+                    wrapping_mul(PRIME2).wrapping_add(v2).
+                    rotate_left(13).wrapping_mul(PRIME1);
+                v3 = read_u32_le(buf, pos + 8).
+                    wrapping_mul(PRIME2).wrapping_add(v3).
+                    rotate_left(13).wrapping_mul(PRIME1);
+                v4 = read_u32_le(buf, pos + 12).
+                    wrapping_mul(PRIME2).wrapping_add(v4).
+                    rotate_left(13).wrapping_mul(PRIME1);
+                pos += 16;
+                if pos > limit {
+                    break;
+                }
+            }
+            self.v1 = v1;
+            self.v2 = v2;
+            self.v3 = v3;
+            self.v4 = v4;
+        }
+        let mut h32: u32;
+        if self.total & 0xf != 0 {
+            return Err(Error::new(ErrorKind::Other, "Wrong call sequence"));
+        }
+        self.total += len;
+        if self.total >= 16 {
+            h32 = self.v1.rotate_left(1).
+                wrapping_add(self.v2.rotate_left(7)).
+                wrapping_add(self.v3.rotate_left(12)).
+                wrapping_add(self.v4.rotate_left(18));
+        } else {
+            h32 = self.v3.wrapping_add(PRIME5);
+        }
+        h32 = h32.wrapping_add(self.total as u32);
+        while pos + 4 <= end {
+            h32 = read_u32_le(buf, pos).
+                wrapping_mul(PRIME3).wrapping_add(h32).
+                rotate_left(17).wrapping_mul(PRIME4);
+            pos += 4;
+        }
+        while pos < end {
+            h32 = (buf[pos] as u32).
+                wrapping_mul(PRIME5).wrapping_add(h32).
+                rotate_left(11).wrapping_mul(PRIME1);
+            pos += 1;
+        }
+        h32 = (h32 ^ (h32 >> 15)).wrapping_mul(PRIME2);
+        h32 = (h32 ^ (h32 >> 13)).wrapping_mul(PRIME3);
+        return Ok(h32 ^ (h32 >> 16));
     }
-    while pos < end {
-        h32 = (buf[pos] as u32).
-            wrapping_mul(prime5).wrapping_add(h32).
-            rotate_left(11).wrapping_mul(prime1);
-        pos += 1;
-    }
-    h32 = (h32 ^ (h32 >> 15)).wrapping_mul(prime2);
-    h32 = (h32 ^ (h32 >> 13)).wrapping_mul(prime3);
-    return h32 ^ (h32 >> 16);
 }
